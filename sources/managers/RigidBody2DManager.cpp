ad639ce44de32ad0d988c8fe66581f5904231cf1
Guillaume S <guillaume.soisson.69@gmail.com>
2022-03-18 22:49:10 +0100

    Add mass in rigidbody calculation

--- sources/managers/RigidBody2DManager.cpp
+++ sources/managers/RigidBody2DManager.cpp
@@ -24,13 +24,15 @@ void sw::RigidBody2DManager::onUpdate()
         sw::Vector2f vel = {obj->getVelocity().x, obj->getVelocity().y};
 
         vel.x = obj->getVelocity().x * 100;
-        vel.y += gravity * currentTime * 100;
+        vel.y += gravity / obj->getMass() * currentTime * 100;
         if (obj->m_collideX == sw::CollisionEvent::LEFT && vel.x < 0)
             vel.x = 0;
         if (obj->m_collideX == sw::CollisionEvent::RIGHT && vel.x > 0)
             vel.x = 0;
 
         if (obj->m_collideY == sw::CollisionEvent::DOWN && vel.y > 0)
+            vel.y = 0;
+        if (obj->m_collideY == sw::CollisionEvent::UP && vel.y < 0)
             vel.y = 0;
         obj->setVelocity(vel);
         transform.move(vel.x * currentTime, vel.y * currentTime);
