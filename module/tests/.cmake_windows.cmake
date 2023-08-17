c169d077220ec81916459b8b2f14bdac3105c3fc
Guilaume S <guillaume.soisson.69@gmail.com>
2023-08-17 15:56:45 +0200

    Remove JSNP reference into cmake

--- module/tests/.cmake_windows.cmake
+++ module/tests/.cmake_windows.cmake
@@ -16,7 +16,6 @@ endif()
 ## IMPORTED SHARED LIBRARY NAME
 set(SHARED_LIB_NAME
         glfw3
-        Jsnp${DEBUG_SUFFIX}
         freetype
         sndfile
         assimp-vc134-mtd
@@ -26,7 +25,6 @@ set(SHARED_LIB_NAME
 ## IMPORTED SHARED LIBRARY LOCATION
 set(SHARED_LIB_LOC
         ${CMAKE_SOURCE_DIR}/../../libraries/${CMAKE_BUILD_TYPE}/glfw3.dll
-        ${CMAKE_SOURCE_DIR}/../../libraries/${CMAKE_BUILD_TYPE}/Jsnp.dll
         ${CMAKE_SOURCE_DIR}/../../libraries/${CMAKE_BUILD_TYPE}/freetype.dll
         ${CMAKE_SOURCE_DIR}/../../libraries/${CMAKE_BUILD_TYPE}/sndfile.dll
         ${CMAKE_SOURCE_DIR}/../../libraries/${CMAKE_BUILD_TYPE}/assimp-vc143-mt${DEBUG_SUFFIX}.dll
@@ -36,7 +34,6 @@ set(SHARED_LIB_LOC
 ## IMPORTED SHARED LIBRARY .lib file
 set( SHARED_LIB
         ${CMAKE_SOURCE_DIR}/../../libraries/${CMAKE_BUILD_TYPE}/glfw3.lib
-        ${CMAKE_SOURCE_DIR}/../../libraries/${CMAKE_BUILD_TYPE}/Jsnp${DEBUG_SUFFIX}.lib
         ${CMAKE_SOURCE_DIR}/../../libraries/${CMAKE_BUILD_TYPE}/freetype.lib
         ${CMAKE_SOURCE_DIR}/../../libraries/${CMAKE_BUILD_TYPE}/sndfile.lib
         ${CMAKE_SOURCE_DIR}/../../libraries/${CMAKE_BUILD_TYPE}/assimp-vc143-mt${DEBUG_SUFFIX}.lib
