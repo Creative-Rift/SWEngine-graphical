b9e02795401285c86f2dae76ff30a1a045eb56f4
Guillaume S <guillaume.soisson.69@gmail.com>
2023-09-26 15:46:04 +0200

    Update cmake Update tests folder

--- module/cmake/windows.cmake
+++ module/cmake/windows.cmake
@@ -1,6 +1,6 @@
 ## PROJECT VAR
 ## <=====================================>
-set( EXEC SWEngine-tests )
+set( EXEC SWEngine-OpenGLModule )
 set( EXT cpp )
 
 add_definitions(-DYAML_CPP_STATIC_DEFINE)
@@ -10,34 +10,33 @@ add_definitions(-DBOOST_CONTAINER_NO_LIB)
 if (${CMAKE_BUILD_TYPE} STREQUAL Debug)
     set(DEBUG_SUFFIX "d")
 else()
-    set(DEBUG_SUFFIX "")
+    unset(DEBUG_SUFFIX)
 endif()
 
 ## IMPORTED SHARED LIBRARY NAME
-set(SHARED_LIB_NAME
+set( SHARED_LIB_NAME
         glfw3
         freetype
         sndfile
-        assimp-vc134-mtd
+        assimp-vc143-mtd
         OpenAL32
         )
 
 ## IMPORTED SHARED LIBRARY LOCATION
-set(SHARED_LIB_LOC
-        ${CMAKE_SOURCE_DIR}/../../libraries/${CMAKE_BUILD_TYPE}/glfw3.dll
-        ${CMAKE_SOURCE_DIR}/../../libraries/${CMAKE_BUILD_TYPE}/freetype.dll
-        ${CMAKE_SOURCE_DIR}/../../libraries/${CMAKE_BUILD_TYPE}/sndfile.dll
-        ${CMAKE_SOURCE_DIR}/../../libraries/${CMAKE_BUILD_TYPE}/assimp-vc143-mt${DEBUG_SUFFIX}.dll
-        ${CMAKE_SOURCE_DIR}/../../libraries/openal/OpenAL32.dll
+set( SHARED_LIB_LOCATION
+        ${CMAKE_SOURCE_DIR}/libraries/${CMAKE_BUILD_TYPE}/glfw3.dll
+        ${CMAKE_SOURCE_DIR}/libraries/${CMAKE_BUILD_TYPE}/freetype.dll
+        ${CMAKE_SOURCE_DIR}/libraries/${CMAKE_BUILD_TYPE}/sndfile.dll
+        ${CMAKE_SOURCE_DIR}/libraries/${CMAKE_BUILD_TYPE}/assimp-vc143-mt${DEBUG_SUFFIX}.dll
+        ${CMAKE_SOURCE_DIR}/libraries/openal/OpenAL32.dll
         )
-
 ## IMPORTED SHARED LIBRARY .lib file
 set( SHARED_LIB
-        ${CMAKE_SOURCE_DIR}/../../libraries/${CMAKE_BUILD_TYPE}/glfw3.lib
-        ${CMAKE_SOURCE_DIR}/../../libraries/${CMAKE_BUILD_TYPE}/freetype.lib
-        ${CMAKE_SOURCE_DIR}/../../libraries/${CMAKE_BUILD_TYPE}/sndfile.lib
-        ${CMAKE_SOURCE_DIR}/../../libraries/${CMAKE_BUILD_TYPE}/assimp-vc143-mt${DEBUG_SUFFIX}.lib
-        ${CMAKE_SOURCE_DIR}/../../libraries/openal/OpenAL32.lib
+        ${CMAKE_SOURCE_DIR}/libraries/${CMAKE_BUILD_TYPE}/glfw3.lib
+        ${CMAKE_SOURCE_DIR}/libraries/${CMAKE_BUILD_TYPE}/freetype.lib
+        ${CMAKE_SOURCE_DIR}/libraries/${CMAKE_BUILD_TYPE}/sndfile.lib
+        ${CMAKE_SOURCE_DIR}/libraries/${CMAKE_BUILD_TYPE}/assimp-vc143-mt${DEBUG_SUFFIX}.lib
+        ${CMAKE_SOURCE_DIR}/libraries/openal/OpenAL32.lib
         )
 
 ## IMPORTED STATIC LIBRARY NAME
@@ -47,8 +46,10 @@ set( STATIC_LIB_NAME
 
 ## IMPORTED STATIC LIBRARY .lib file
 set( STATIC_LIB
-        ${CMAKE_SOURCE_DIR}/../../libraries/${CMAKE_BUILD_TYPE}/yaml-cpp${DEBUG_SUFFIX}.lib
+        ${CMAKE_SOURCE_DIR}/libraries/${CMAKE_BUILD_TYPE}/yaml-cpp${DEBUG_SUFFIX}.lib
         )
+## <=====================================>
+
 
 ## GET SOURCES
 ## <=====================================>
@@ -59,19 +60,11 @@ endforeach()
 file(GLOB SRC ${TMP})
 ## <=====================================>
 
-
 ## OUTPUT
 ## <=====================================>
-## EXECUTABLE
-add_executable(${EXEC} ${SRC})
-
-## or SHARED LIB
-#add_library(${EXEC} SHARED ${SRC})
-
-## or STATIC LIB
-# add_library(${EXEC} STATIC ${SRC})
+## Build shared lib
+add_library(${EXEC} SHARED ${SRC})
 ## <=====================================>
-
 
 ## ADD INCLUDES
 ## <=====================================>
@@ -84,7 +77,6 @@ if(MSVC)
     target_compile_options(${EXEC} PRIVATE "/MP")
 endif()
 ## <=====================================>
-
 
 ## SHARED LIBRARY LINKING
 ## <=====================================>
@@ -130,30 +122,3 @@ if (${CMAKE_BUILD_TYPE} MATCHES Debug)
     set_target_properties(${EXEC} PROPERTIES
             DEBUG_POSTFIX "d")
 endif()
-
-# Avoid warning about DOWNLOAD_EXTRACT_TIMESTAMP in CMake 3.24:
-if (CMAKE_VERSION VERSION_GREATER_EQUAL "3.24.0")
-    cmake_policy(SET CMP0135 NEW)
-endif()
-
-## PACKAGE LINKING
-## <=====================================>
-include(FetchContent)
-FetchContent_Declare(
-        googletest
-        URL https://github.com/google/googletest/archive/609281088cfefc76f9d0ce82e1ff6c30cc3591e5.zip
-)
-
-set(gtest_force_shared_crt ON CACHE BOOL "" FORCE)
-FetchContent_MakeAvailable(googletest)
-
-enable_testing()
-
-target_link_libraries(${EXEC}
-        PUBLIC
-        gtest_main
-        )
-
-include(GoogleTest)
-gtest_discover_tests(${EXEC})
-## <=====================================>
